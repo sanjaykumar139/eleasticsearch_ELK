2ea37945ea229d07ff657f48a3e2f92b5491fac6
sanjay.kumar <sanjay.kumar@taazaa.com>
2023-07-23 22:08:03 +0530

    elastic search c#

---
+++ ElasticDemo2/ElasticDemo2/Model/ArticleModel.cs
@@ -0,0 +1,36 @@
+﻿namespace ElasticDemo2.Model
+{
+    public class ArticleModel
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Title
+        {
+            get;
+            set;
+        }
+        public string Link
+        {
+            get;
+            set;
+        }
+        public string Author
+        {
+            get;
+            set;
+        }
+        public string AuthorLink
+        {
+            get;
+            set;
+        }
+        public DateTime PublishedDate
+        {
+            get;
+            set;
+        }
+    }
+}
