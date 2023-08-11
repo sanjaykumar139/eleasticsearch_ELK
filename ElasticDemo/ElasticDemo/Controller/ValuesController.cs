fecae39b03c9f075dbeb86d9e3671e2be08f2260
sanjay.kumar <sanjay.kumar@taazaa.com>
2023-08-11 21:11:04 +0530

    elastic search demo

---
+++ ElasticDemo/ElasticDemo/Controller/ValuesController.cs
@@ -0,0 +1,85 @@
+﻿using ElasticDemo.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using Newtonsoft.Json;
+
+namespace ElasticDemo.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IElasticClient _elasticClient;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public ValuesController(IElasticClient elasticClient, IWebHostEnvironment hostingEnvironment)
+        {
+            _elasticClient = elasticClient;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+
+        [HttpPost]
+        public async Task<string> Create(ArticleModel model)
+        {
+            string strResult= string.Empty;
+            try
+            {
+                var article = new ArticleModel()
+                {
+                    Id = 1,
+                    Title = model.Title,
+                    Link = model.Link,
+                    Author = model.Author,
+                    AuthorLink = model.AuthorLink,
+                    PublishedDate = DateTime.Now
+                };
+                await _elasticClient.IndexDocumentAsync(article);
+                model = new ArticleModel();
+            }
+            catch (Exception ex) { strResult = "failed"; }
+            return strResult = "success";
+        }
+
+        [HttpPost]
+        public string Import()
+        {
+            string strResult = string.Empty;
+            try
+            {
+                var rootPath = _hostingEnvironment.ContentRootPath; //get the root path
+                var fullPath = Path.Combine(rootPath, "articles.json"); //combine the root path with that of our json file inside mydata directory
+                var jsonData = System.IO.File.ReadAllText(fullPath); //read all the content inside the file
+                var articleList = JsonConvert.DeserializeObject<List<ArticleModel>>(jsonData);
+                if (articleList != null)
+                {
+                    foreach (var article in articleList)
+                    {
+                        _elasticClient.IndexDocumentAsync(article);
+                    }
+                }
+            }
+            catch (Exception ex) { strResult = "failed"; }
+            return strResult = "success";
+        }
+
+        [HttpGet(Name= "GetSearch")]
+        public IList<ArticleModel> GetSearch(string keyword)
+        {
+            var result = _elasticClient.SearchAsync<ArticleModel>(s => s.Query(q => q.QueryString(d => d.Query('*' + keyword + '*'))).Size(5000));
+            var finalResult = result;
+            var finalContent = finalResult.Result.Documents.ToList();
+            return finalContent;
+        }
+
+        //public ActionResult Index(string keyword)
+        //{
+        //    var articleList = new List<ArticleModel>();
+        //    if (!string.IsNullOrEmpty(keyword))
+        //    {
+        //        articleList = GetSearch(keyword).ToList();
+        //    }
+        //    return View(articleList.AsEnumerable());
+        //}
+    }
+}
---
+++ ElasticDemo/ElasticDemo/Service/ElasticSearchExtension.cs
@@ -0,0 +1,28 @@
+﻿using ElasticDemo.Model;
+using Nest;
+
+namespace ElasticDemo.Service
+{
+    public static class ElasticSearchExtension
+    {
+        public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            var baseUrl = configuration["ElasticSettings:baseUrl"];
+            var index = configuration["ElasticSettings:defaultIndex"];
+            var settings = new ConnectionSettings(new Uri(baseUrl ?? "")).PrettyJson().CertificateFingerprint("6b6a8c2ad2bc7b291a7363f7bb96a120b8de326914980c868c1c0bc6b3dc41fd").BasicAuthentication("elastic", "*9HN4g8-DZZ+UcdnlKja").DefaultIndex(index);
+            settings.EnableApiVersioningHeader();
+            AddDefaultMappings(settings);
+            var client = new ElasticClient(settings);
+            services.AddSingleton<IElasticClient>(client);
+            CreateIndex(client, index);
+        }
+        private static void AddDefaultMappings(ConnectionSettings settings)
+        {
+            settings.DefaultMappingFor<ArticleModel>(m => m.Ignore(p => p.Link).Ignore(p => p.AuthorLink));
+        }
+        private static void CreateIndex(IElasticClient client, string indexName)
+        {
+            var createIndexResponse = client.Indices.Create(indexName, index => index.Map<ArticleModel>(x => x.AutoMap()));
+        }
+    }
+}
